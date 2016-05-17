b621ee14cedd9f6fb30b36299a9135f6a0dd49d1
Timmy Nguyen <Nguyen17@users.noreply.github.com>
2016-05-17 13:02:49 -0700

    Add files via upload

---
+++ sqrt.cpp
@@ -0,0 +1,18 @@
+/*----------------------------------
+Program: The Sqrt Function
+Problem Question:
+Desc: Learn how to use a pre existing sqrt() function
+Date: May 17, 2016
+
+-----------------------------------*/
+
+#include <iostream>
+#include <cmath> //calls the sqrt() from cmath
+using namespace std;
+
+int main()
+{
+    double four = 4;
+
+    cout << "The sqrt of 4 is: " << sqrt(four) << endl;
+}
