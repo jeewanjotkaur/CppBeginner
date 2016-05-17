314640fdd09a38e8531d0e3338cec65d0ea45c21
Timmy Nguyen <message.timmynguyen@gmail.com>
2016-05-17 11:50:43 -0800

    added a file

---
+++ variables.cpp
@@ -0,0 +1,24 @@
+/*-----------------------------------
+Program: Learning Variables
+Problem Qustion: Create a variable and output it.
+Desc: Learning about cin and variables
+Date: May 17, 2016
+
+----------------------------------- */
+# include <iostream>
+using namespace std;
+
+int main()
+{
+    int apples = 4; // declaration statement, type + variable name
+    //initialize with 4
+
+    cout << "I have " << apples << endl;
+
+    cout << "I ate 1...." << endl;
+
+    apples = apples - 1; // variable modified
+
+    cout << "Now i have " << apples << " left" << endl;
+
+}
